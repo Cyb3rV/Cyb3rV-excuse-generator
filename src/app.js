64a5853102bdb330dbf4891639c9462597c31f7f
Cyb3rV <152754161+Cyb3rV@users.noreply.github.com>
2024-02-08 01:57:59 +0000

    Finished project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  const a = who[getRandomInt(who.length)];
+  const b = action[getRandomInt(action.length)];
+  const c = what[getRandomInt(what.length)];
+  const d = when[getRandomInt(when.length)];
+
+  //Create a random excuse
+  var excuse = "Hello this is a random excuse!!";
+
+  excuse = `${a} ${b} ${c} ${d}`;
+
+  //change the HTML page to show the random excuse
+  document.querySelector("div").innerText = excuse;
 };
